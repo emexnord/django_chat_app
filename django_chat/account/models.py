542ae552832d388ef33281bc2d253412c3623390
Emran Kamil <emranhi001@gmail.com>
2025-03-26 11:48:44 +0300

    changed account model

--- django_chat/account/models.py
+++ django_chat/account/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Account(AbstractUser):
-    pass
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    is_online = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
