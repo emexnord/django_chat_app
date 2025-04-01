b3be5daa858e212a2691f6b48887176e233416d4
Emran Kamil <emranhi001@gmail.com>
2025-04-01 22:28:46 +0300

    start test codes

--- django_chat/account/tests.py
+++ django_chat/account/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, "testuser")
+        self.assertTrue(self.user.check_password("testpassword"))
+
+    def test_user_str_representation(self):
+        self.assertEqual(str(self.user), "testuser")
