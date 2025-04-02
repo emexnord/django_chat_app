fcabc899895880a9a93622981205ef9ad5914b2c
Emran Kamil <emranhi001@gmail.com>
2025-04-02 21:47:36 +0300

    minor change to test code

--- django_chat/account/tests.py
+++ django_chat/account/tests.py
@@ -14,3 +14,9 @@ class UserModelTest(TestCase):
 
     def test_user_str_representation(self):
         self.assertEqual(str(self.user), "testuser")
+
+    def test_user_removal(self):
+        user_id = self.user.id
+        self.user.delete()
+        with self.assertRaises(User.DoesNotExist):
+            User.objects.get(id=user_id)
