1a1f5b73d25b1b31bc4a18a6b19a60370813e6e6
Emran Kamil <emranhi001@gmail.com>
2025-05-12 23:18:18 +0300

    added test code for the server

--- django_chat/server/tests.py
+++ django_chat/server/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .tests import *  # Import everything from tests.py to test
+
+
+class ExampleTestCase(TestCase):
+    def test_example(self):
+        # Example test to ensure the test framework is working
+        self.assertEqual(1 + 1, 2)
