735d74a85df0f1be6cf6eb552154ad381895ef69
Emran Kamil <emranhi001@gmail.com>
2025-05-26 22:36:58 +0300

    added test codes for server of the chat app

--- django_chat/server/tests.py
+++ django_chat/server/tests.py
@@ -7,3 +7,19 @@ class ExampleTestCase(TestCase):
     def test_example(self):
         # Example test to ensure the test framework is working
         self.assertEqual(1 + 1, 2)
+
+        def test_false_is_not_true(self):
+            self.assertFalse(False)
+
+        def test_uppercase(self):
+            self.assertEqual("chat".upper(), "CHAT")
+
+        def test_isupper(self):
+            self.assertTrue("CHAT".isupper())
+            self.assertFalse("Chat".isupper())
+
+        def test_split(self):
+            s = "hello world"
+            self.assertEqual(s.split(), ["hello", "world"])
+            with self.assertRaises(TypeError):
+                s.split(2)
