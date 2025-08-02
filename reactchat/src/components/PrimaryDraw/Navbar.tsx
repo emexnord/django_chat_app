5be1e0670ebb40ddcfd957edf3fe586d1b27b202
Emran Kamil <128908090+emexnord@users.noreply.github.com>
2025-08-02 21:55:25 +0300

    Update Navbar.tsx

--- reactchat/src/components/PrimaryDraw/Navbar.tsx
+++ reactchat/src/components/PrimaryDraw/Navbar.tsx
@@ -10,10 +10,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-blue-600 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
-          <div className="flex-shrink-0">
-            <h1 className="text-xl font-bold">ReactChat</h1>
-          </div>
+        <div className="flex justify-between items-center h-16">\
           <div className="hidden md:flex space-x-4">
             <a
               href="#"
