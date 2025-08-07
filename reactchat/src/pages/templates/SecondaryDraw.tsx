c59745f4e131f7a1bda2d80abe186b816f37c2ba
Emran Kamil <128908090+emexnord@users.noreply.github.com>
2025-08-07 22:03:43 +0300

    make changes to secondary drawer

--- reactchat/src/pages/templates/SecondaryDraw.tsx
+++ reactchat/src/pages/templates/SecondaryDraw.tsx
@@ -1,11 +1,1 @@
-import { Box, Typography } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
 
-const SecondaryDraw = () => {  
-    const theme = useTheme()
-    return (
-        
-    );
-}
-
-export default SecondaryDraw;
